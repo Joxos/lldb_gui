d95b50bf8f0a612d00a6197dde6e57fff5a194eb
Joxos <johndoe@example.com>
2022-08-15 20:00:11 +0800

    update

--- lldb_gui.py
+++ lldb_gui.py
@@ -26,16 +26,31 @@ def show_message(message):
     msgBox.exec()
 
 
+def log_and_show_message(message, level="error"):
+    show_message(message)
+    if level == "error":
+        logger.error(message)
+    elif level == "info":
+        logger.info(message)
+    else:
+        raise ValueError("level must be \"error\" or \"info\"")
+
+
 class Window(QWidget):
 
     def __init__(self):
         super().__init__()
 
+        # load ui
         qfile = QtCore.QFile("lldb_gui.ui")
         self.ui = QUiLoader().load(qfile)
-        self.ui.attach_lldb.clicked.connect(self.attach_lldb)
-        self.ui.run_exec.clicked.connect(self.run_exec)
+
+        # init variables
         self.target = None
+        self.breakpoints = []
+        self.base_path = self.ui.base_path.placeholderText()
+        self.exec_path = ""
+        self.full_path = self.base_path + self.exec_path
 
         # init breakpoints table
         self.ui.breakpoints.setColumnCount(4)
@@ -47,27 +62,54 @@ class Window(QWidget):
         self.ui.breakpoints.verticalHeader().setSectionResizeMode(
             QHeaderView.Stretch)
 
+        # connect
+        self.ui.attach_lldb.clicked.connect(self.attach_lldb)
+        self.ui.run_exec.clicked.connect(self.run_exec)
+
     @QtCore.Slot()
     def attach_lldb(self):
+        # get variables
         global debugger
+        base_path = self.ui.base_path.toPlainText()
+        if len(base_path) > 0:
+            if base_path[-1] not in ["\\", "/"]:
+                base_path += "/"
         exec_path = self.ui.exec_path.toPlainText()
+        full_path = base_path + exec_path
+        logger.debug(f"Base path: {base_path}")
         logger.debug(f"Exec path: {exec_path}")
-        if not os.path.isfile(exec_path):
-            show_message(
-                f"Executable {exec_path} doesn't exsist! LLDB process is now the same as before."
+        logger.debug(f"Full path: {full_path}")
+
+        # verify executable file
+        if not os.path.isfile(full_path):
+            log_and_show_message(
+                f"{full_path} isn't a file. LLDB process is now the same as before."
             )
-            logger.error(f"{exec_path} isn't a file. Stopped operation!")
             return
+
+        # create target
         self.target = debugger.CreateTargetWithFileAndArch(
-            exec_path, lldb.LLDB_ARCH_DEFAULT)
+            full_path, lldb.LLDB_ARCH_DEFAULT)
         # LLDB failed to attach the target
         if not self.target:
-            show_message(
-                f"LLDB failed to attach {exec_path}. LLDB process is now the same as before."
+            log_and_show_message(
+                f"LLDB failed to attach {full_path}. LLDB process is now the same as before."
             )
-            logger.error(f"Failed to attach {exec_path}")
-        self.ui.attach_lldb.setText("Reattach")
+
+        # update variables
         self.exec_path = exec_path
+        self.base_path = base_path
+        self.full_path = full_path
+
+        # enable buttons
+        self.ui.run_exec.setEnabled(True)
+        self.ui.stop_exec.setEnabled(True)
+        self.ui.step_into.setEnabled(True)
+        self.ui.step_over.setEnabled(True)
+        self.ui.continue_exec.setEnabled(True)
+        self.ui.add_breakpoint.setEnabled(True)
+        # update attatch button to "Reattach"
+        self.ui.attach_lldb.setText("Reattach")
 
     @QtCore.Slot()
     def run_exec(self):
@@ -75,8 +117,14 @@ class Window(QWidget):
             self.process = self.target.LaunchSimple(None, None, os.getcwd())
             logger.debug(f"Successfully run {self.exec_path}")
             return
-        show_message("There isn't any target to run.")
-        logger.error("There isn't any target.")
+        log_and_show_message("There isn't any target.")
+
+    @QtCore.Slot()
+    def add_breakpoint(self):
+        # messagebox to get data
+        # create breakpoint and add it to self.breakpoints
+        # redisplay self.ui.breakpoints
+        pass
 
 
 if __name__ == "__main__":
@@ -85,3 +133,11 @@ if __name__ == "__main__":
     w.setWindowTitle("LLDB Debugger GUI")
     w.ui.show()
     app.exec()
+"""
+Testing:
+1. base path not set and exec path is wrong
+2. base path is set without any delimiter and exec_path is wrong
+   take a look at full_path in console
+3. correct path to an executable file
+   then run it
+"""
