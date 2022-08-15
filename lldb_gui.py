e21ca49d1a11d9106e6f48b8c94dfc58e1adc442
Joxos <johndoe@example.com>
2022-08-15 13:47:08 +0800

    show_message and run_exec

--- lldb_gui.py
+++ lldb_gui.py
@@ -15,8 +15,15 @@ logger.debug(f"Succeeded to import module lldb at {lldb_path}")
 logger.debug("Initializing LLDB...")
 global debugger
 debugger = lldb.SBDebugger.Create()
-# debugger.setAsync(False)
 logger.debug("Finished initializing lldb.")
+
+
+def show_message(message):
+    msgBox = QMessageBox()
+    msgBox.setText(message)
+    msgBox.setStandardButtons(QMessageBox.Ok)
+    msgBox.setDefaultButton(QMessageBox.Ok)
+    msgBox.exec()
 
 
 class Window:
@@ -26,61 +33,42 @@ class Window:
 
         qfile = QFile("lldb_gui.ui")
         self.ui = QUiLoader().load(qfile)
-        # self.ui.button.clicked.connect(self.btnClick)
         self.ui.attach_lldb.clicked.connect(self.attach_lldb)
         self.ui.run_exec.clicked.connect(self.run_exec)
+        self.target = None
 
     def attach_lldb(self):
         global debugger
-        self.exec_path = self.ui.exec_path.toPlainText()
-        logger.debug(f"Exec path: {self.exec_path}")
-        if not os.path.isfile(self.exec_path):
-            # show_message(
-            #     f"Executable {exec_path} doesn't exsist! LLDB process is now unstarted"
-            # )
-            logger.error(f"{self.exec_path} isn't a file. Stopped operation!")
+        exec_path = self.ui.exec_path.toPlainText()
+        logger.debug(f"Exec path: {exec_path}")
+        if not os.path.isfile(exec_path):
+            show_message(
+                f"Executable {exec_path} doesn't exsist! LLDB process is now the same as before."
+            )
+            logger.error(f"{exec_path} isn't a file. Stopped operation!")
             return
         self.target = debugger.CreateTargetWithFileAndArch(
-            self.exec_path, lldb.LLDB_ARCH_DEFAULT)
+            exec_path, lldb.LLDB_ARCH_DEFAULT)
+        # LLDB failed to attach the target
         if not self.target:
-            logger.error(f"Failed to attach {self.exec_path}")
+            show_message(
+                f"LLDB failed to attach {exec_path}. LLDB process is now the same as before."
+            )
+            logger.error(f"Failed to attach {exec_path}")
+        self.ui.attach_lldb.setText("Reattach")
+        self.exec_path = exec_path
 
     def run_exec(self):
-        self.process = self.target.LaunchSimple(None, None, os.getcwd())
-        logger.debug(f"Successfully run {self.exec_path}")
-        logger.debug(f"Successfully run {self.exec_path}")
+        if self.target:
+            self.process = self.target.LaunchSimple(None, None, os.getcwd())
+            logger.debug(f"Successfully run {self.exec_path}")
+            return
+        show_message("There isn't any target to run.")
+        logger.error("There isn't any target.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication([])
     w = Window()
     w.ui.show()
     app.exec()
-
-# import os, subprocess, sys
-
-# gui = Gui(["LLDB GUI Debug Tool", _, "Author Joxos"],
-#           ["LLDB Process Status: ", ("Unactivated", "status"), _],
-#           ["Executable Path:", "__exec_path__", ["Run LLDB"]],
-#           [(["Run Executable"], "run"), _, _])
-# global p_lldb
-# p_lldb = None
-# logger.debug("Initialize done.")
-
-# gui.RunLLDB = run_lldb
-
-# def run_exec(gui, *args):
-#     global p_lldb
-#     if p_lldb == None:
-#         show_message("LLDB process hasn't started yet!")
-#         return
-#     print(p_lldb.communicate("run"))
-
-# gui.run = run_exec
-
-# gui.run()
-# # Ensure that lldb process is terminated.
-# # if p_lldb != None:
-# #     p_lldb.terminate()
-# #     logger.debug("LLDB process terminated. Really exit now.")
-# # logger.debug("LLDB process hasn't started yet. Exit directly.")
